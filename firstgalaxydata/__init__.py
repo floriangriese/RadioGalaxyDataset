20a83a6542620637ded3f4db8f642cd1df357cdb
Florain Griese <florian.griese@tuhh.de>
2022-09-29 17:12:55 +0200

    preparation for pypi package, not working yet

---
+++ firstgalaxydata/__init__.py
@@ -0,0 +1,1 @@
+from .firstgalaxydata import FIRSTGalaxyData
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='firstgalaxydata',
+      version='0.1',
+      description='Package to load radio galaxy data',
+      author='Florian Griese',
+      packages=['firstgalaxydata'],
+      zip_safe=False)
